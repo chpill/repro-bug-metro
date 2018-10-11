c91701fe145b42d0ee46db6ef05c6e0d9f1f8c4c
chpill <etienne@spillemaeker.com>
2018-10-11 16:46:37 +0200

    Fix extern munging

--- build.clj
+++ build.clj
@@ -6,17 +6,18 @@
                       :output-to     "index.js"
                       :source-map    "index.js.map"
                       :target        :nodejs
-                      :optimizations :simple
+                      :infer-externs true
 
-                      :language-in :ecmascript5
-                      :language-out :no-transpile
-
+                      :optimizations :advanced
 
 
                       :pseudo-names true
 
 
 
+                      :compiler-stats true
                       :parallel-build true})
 
 (api/build source-dir compiler-config)
+
+
